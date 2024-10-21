d7a9f01f50ad6ab4e71d89932e273c2dadaeb709
Shuvankar Dhal <dhalshuvankar@gmail.com>
2024-10-21 19:08:11 +0530

    added changes

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,6 +9,8 @@ test('assert the transactions count', async ({ page }) => {
 
 test('assert input-output', async ({ page }) => {
   await page.goto('https://blockstream.info/block/000000000000000000076c036ff5119e5a5a74df77abf64203473364509f7732');
-
+  var inputnums = page.locator('.vins').children('div').length;
+  var outputNums= page.locator('.vouts').children('div').length;
+  
   
 });
